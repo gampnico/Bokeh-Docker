fa9542a20dbad1a5f036e74237cd4256c9931c88
gampnico <45390064+gampnico@users.noreply.github.com>
2025-09-25 12:51:04 +0100

    fix: branch pointer

--- runner/run.sh
+++ runner/run.sh
@@ -2,7 +2,7 @@
 set -xe
 
 rm -rf repo
-git clone https://github.com/gampnico/Bokeh-Docker.git@fix-bokeh-dash repo
+git clone --branch fix-bokeh-dash https://github.com/gampnico/Bokeh-Docker.git repo
 cd repo/bokeh.oggm.org
 
 docker compose down --remove-orphans

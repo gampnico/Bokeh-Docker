7161b8e3e528584f6b10713c2fcdae3d5bf344f0
gampnico <45390064+gampnico@users.noreply.github.com>
2026-01-20 12:49:44 +0000

    fix: typo

--- runner/run.sh
+++ runner/run.sh
@@ -2,7 +2,7 @@
 set -xe
 
 rm -rf repo
-git clone --b local-runner https://github.com/gampnico/Bokeh-Docker.git repo
+git clone -b local-runner https://github.com/gampnico/Bokeh-Docker.git repo
 cd repo/bokeh.oggm.org
 
 docker compose down --remove-orphans
